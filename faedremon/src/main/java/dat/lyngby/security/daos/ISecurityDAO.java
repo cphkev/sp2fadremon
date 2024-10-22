213d2b0bfb407ce0f7c6f9d7981bfa8b4e79c868
ScriptCodes <143606581+ScriptCodes@users.noreply.github.com>
2024-10-22 12:02:41 +0200

    User Methods:
    Search By ID
    
    Get All

--- faedremon/src/main/java/dat/lyngby/security/daos/ISecurityDAO.java
+++ faedremon/src/main/java/dat/lyngby/security/daos/ISecurityDAO.java
@@ -4,8 +4,12 @@ import dk.bugelhartmann.UserDTO;
 import dat.lyngby.security.entities.User;
 import dat.lyngby.security.exceptions.ValidationException;
 
+import java.util.List;
+
 public interface ISecurityDAO {
     UserDTO getVerifiedUser(String username, String password) throws ValidationException;
     User createUser(String username, String password);
-    User createUserOG(String username, String password);
+    //User createUserOG(String username, String password);
+    User getUserByID(int id);
+    List<User> getAll();
 }
