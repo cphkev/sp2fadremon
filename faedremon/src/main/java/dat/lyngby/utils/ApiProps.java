32b6c57c6bac2ebeaf385df298572f2e0a3db9e2
cphkev <143606716+cphkev@users.noreply.github.com>
2024-10-22 11:54:09 +0200

    ApiProps med mads port

--- faedremon/src/main/java/dat/lyngby/utils/ApiProps.java
+++ faedremon/src/main/java/dat/lyngby/utils/ApiProps.java
@@ -8,10 +8,11 @@ import lombok.NoArgsConstructor;
 public class ApiProps {
 
     // == HIBERNATE CONFIG FILE ==
-    public static final String DB_NAME = "hotel";
+    public static final String DB_NAME = "sp2faedremon";
     public static final String DB_USER = "postgres";
     public static final String DB_PASS = "postgres";
     public static final String DB_URL = "jdbc:postgresql://localhost:5432/" + DB_NAME;
+    public static final String DB_URL_MADS ="jdbc:postgresql://localhost:5433/" + DB_NAME;
 
     // == API CONFIG ==
     public static final int PORT = 7007;
