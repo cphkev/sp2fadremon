c4ee77fac43c98d5f6ab9bf7867e3d332e5f370e
cphkev <143606716+cphkev@users.noreply.github.com>
2024-10-24 00:02:48 +0200

    pack routes og routes init

--- faedremon/src/main/java/dat/lyngby/routes/Routes.java
+++ faedremon/src/main/java/dat/lyngby/routes/Routes.java
@@ -8,14 +8,15 @@ import static io.javalin.apibuilder.ApiBuilder.path;
 public class Routes {
 
 
+    private final CardRoutes cardRoutes = new CardRoutes();
+    private final PackRoutes packRoutes = new PackRoutes();
 
 
 
     public EndpointGroup getApiRoutes() {
         return () -> {
-
-
-
+            path("/cards", cardRoutes.getRoutes());
+            path("/packs", packRoutes.getRoutes());
         };
     }
 }
