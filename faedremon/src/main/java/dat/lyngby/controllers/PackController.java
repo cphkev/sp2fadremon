5545856189ceef841d34069858ecf0eb0ab8ab0e
cphkev <143606716+cphkev@users.noreply.github.com>
2024-10-23 12:51:36 +0200

    Controllers og routes

---
+++ faedremon/src/main/java/dat/lyngby/controllers/PackController.java
@@ -0,0 +1,9 @@
+package dat.lyngby.controllers;
+
+/**
+ * Purpose:
+ *
+ * @author: Kevin Løvstad Schou
+ */
+public class PackController {
+}
---
+++ faedremon/src/main/java/dat/lyngby/routes/CardRoutes.java
@@ -0,0 +1,23 @@
+package dat.lyngby.routes;
+
+
+import dat.lyngby.controllers.CardController;
+import io.javalin.apibuilder.EndpointGroup;
+import static io.javalin.apibuilder.ApiBuilder.*;
+public class CardRoutes {
+
+    private final CardController cardController = new CardController();
+
+
+    protected EndpointGroup getRoutes(){
+        return () -> {
+//            post("/", cardController::create);
+//            get("/", cardController::readAll);
+//            get("/{id}", cardController::read);
+//            put("/{id}", cardController::update);
+//            delete("/{id}", cardController::delete);
+        };
+    }
+
+
+}
